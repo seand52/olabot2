3b875937e7cb652ba3faae1a100b7cc49ad47167
Sean <seand52@gmail.com>
2023-05-09 23:13:58 +0100

    Use polling instead of webhook

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,13 @@
 import { Configuration, OpenAIApi } from 'openai';
 
-import ChatBot from 'helpers/chatBot';
 import { FastifyPluginAsync } from 'fastify';
+import TelegramBot from 'node-telegram-bot-api';
 import fs from 'fs';
 import schedule from 'node-schedule'
 import { writeTextToFile } from 'helpers/writeToFile';
 
 const token = process.env.TELEGRAM_API_KEY || ''
-const bot = new ChatBot(token, { polling: false })
-bot.setWebhook('https://0e0d-139-47-41-5.eu.ngrok.io/webhook')
+const bot = new TelegramBot(token, { polling: true })
 
 
 const configuration = new Configuration({
@@ -21,52 +20,50 @@ Nombre: mensaje
 
 Quiero que me hagas un resumen de todos los mensajes que he enviado. El resumen tiene que tener un maximo de 500 palabras. Gracias. Gracias.
 `
-
-schedule.scheduleJob('0 22 * * *', async function() {
-  console.log('olalaaaaaaaaaaaaaaaaaaaa')
-  // const prompt = fs.readFileSync('convo.txt', 'utf-8')
-  // try {
-  //   const completion = await openai.createCompletion({
-  //     model: 'text-davinci-003',
-  //     prompt: `${intro}\n${prompt}`,
-  //     max_tokens: 1000
-  //   })
-  //   console.log({completion: completion.data.choices[0].text})
-  // } catch (e) {
-  //   console.log(e)
-  // }
-  // clearFile()
+bot.on('message', async (msg, match) => {
+  console.log({ msg, match })
+  if (match.type !== 'text') {
+    return
+  }
+  await writeTextToFile(msg)
+});
+schedule.scheduleJob('0 22 * * *', async function () {
+  const prompt = fs.readFileSync('convo.txt', 'utf-8')
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${intro}\n${prompt}`,
+      max_tokens: 1000
+    })
+    console.log({ completion: completion.data.choices[0].text })
+  } catch (e) {
+    console.log(e)
+  }
+  clearFile()
 })
-const clearFile  = () => {
+const clearFile = () => {
   fs.writeFile('convo.txt', '', (err) => {
     if (err) throw err;
     console.log('File cleared!');
   })
 }
+
+
 const routes: FastifyPluginAsync = async (server) => {
   server.get('/', async function (_, res) {
-    // const prompt = fs.readFileSync('convo.txt', 'utf-8')
-    // try {
-    //   const completion = await openai.createCompletion({
-    //     model: 'text-davinci-003',
-    //     prompt: `${intro}\n${prompt}`,
-    //     max_tokens: 1000
-    //   })
-    //   console.log({completion: completion.data.choices[0].text})
-    // } catch (e) {
-    //   console.log(e)
-    // }
-    // clearFile()
-  });
-
-  server.post('/webhook', async (req, res) => {
-    const { message } = req.body as any
-    console.log({ message })
-    if (!message.text) {
-      return
+    const prompt = fs.readFileSync('convo.txt', 'utf-8')
+    try {
+      const completion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${intro}\n${prompt}`,
+        max_tokens: 1000
+      })
+      console.log({ completion: completion.data.choices[0].text })
+    } catch (e) {
+      console.log(e)
     }
-    await writeTextToFile(message)
-  })
+    clearFile()
+  });
 }
 
 export default routes;
